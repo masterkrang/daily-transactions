3cddb2a2fbd83b91144c74969ead128e818c1ca5
Kurtis Braget <kurt@popstand.com>
2019-08-25 01:44:15 -0700

    add uber charting

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+import { Grommet } from 'grommet';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC_ncUjfwVo3Y_FgHHD7hkX3-zuEvyTkBM",
+  authDomain: "daily-transactions-58c7e.firebaseapp.com",
+  databaseURL: "https://daily-transactions-58c7e.firebaseio.com",
+  projectId: "daily-transactions-58c7e",
+  storageBucket: "daily-transactions-58c7e.appspot.com",
+  messagingSenderId: "44563459078",
+  appId: "1:44563459078:web:a9ec48e43d1f1fd4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
